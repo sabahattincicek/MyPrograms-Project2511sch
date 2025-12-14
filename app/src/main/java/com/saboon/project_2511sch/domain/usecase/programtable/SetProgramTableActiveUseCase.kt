6659e76af681d38b01b1f32a72e268a3db65ec53
zabo0 <scicek12345@gmail.com>
2025-12-14 23:51:12 +0300

    refactor: renamed class to SetProgramTableActiveUseCase.kt and add parameter to function

--- app/src/main/java/com/saboon/project_2511sch/domain/usecase/programtable/SetProgramTableActiveUseCase.kt
+++ app/src/main/java/com/saboon/project_2511sch/domain/usecase/programtable/SetProgramTableActiveUseCase.kt
@@ -1,13 +1,14 @@
 package com.saboon.project_2511sch.domain.usecase.programtable
 
+import com.saboon.project_2511sch.domain.model.ProgramTable
 import com.saboon.project_2511sch.domain.repository.IProgramTableRepository
 import com.saboon.project_2511sch.util.Resource
 import javax.inject.Inject
 
-class SetAllProgramTablesInactiveUseCase @Inject constructor(
+class SetProgramTableActiveUseCase @Inject constructor(
     private val programTableRepository: IProgramTableRepository
 ) {
-    suspend operator fun invoke(): Resource<Unit>{
-        return programTableRepository.setInactiveAllProgramTable()
+    suspend operator fun invoke(programTable: ProgramTable): Resource<Unit>{
+        return programTableRepository.setProgramTableActive(programTable)
     }
 }
