fced486a9f2525d3d6401b9d412b3cdf2f76816f
zabo0 <scicek12345@gmail.com>
2025-12-14 22:04:41 +0300

    feat: implement adapter for DialogFragmentProgramTableSelector.kt

--- app/src/main/java/com/saboon/project_2511sch/presentation/home/RecyclerAdapterDialogFragmentHome.kt
+++ app/src/main/java/com/saboon/project_2511sch/presentation/home/RecyclerAdapterDialogFragmentHome.kt
@@ -1,32 +1,56 @@
 package com.saboon.project_2511sch.presentation.home
 
-import android.view.View
+import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.fragment.app.DialogFragment
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.saboon.project_2511sch.databinding.RecyclerListRowDialogFragmentHomeBinding
 import com.saboon.project_2511sch.domain.model.ProgramTable
 
 class RecyclerAdapterDialogFragmentHome : ListAdapter<ProgramTable, RecyclerAdapterDialogFragmentHome.DialogFragmentHomeViewHolder>(
     DialogFragmentHomeDiffCallback()
 ) {
 
-    class DialogFragmentHomeViewHolder(view: View): RecyclerView.ViewHolder(view) {
+    var onItemClickListener: ((ProgramTable) -> Unit)? = null
 
+    class DialogFragmentHomeViewHolder(private val binding: RecyclerListRowDialogFragmentHomeBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: ProgramTable){
+            binding.tvProgramTableTitle.text = item.title
+            binding.tvProgramTableDescription.text = item.description
+        }
     }
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): DialogFragmentHomeViewHolder {
-        TODO("Not yet implemented")
+        val binding = RecyclerListRowDialogFragmentHomeBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return DialogFragmentHomeViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: DialogFragmentHomeViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item = getItem(position)
+        holder.bind(item)
+        holder.itemView.setOnClickListener {
+            onItemClickListener?.invoke(item)
+        }
     }
 
-    open class DialogFragmentHomeDiffCallback {
+    open class DialogFragmentHomeDiffCallback: DiffUtil.ItemCallback<ProgramTable>() {
+        override fun areItemsTheSame(
+            oldItem: ProgramTable,
+            newItem: ProgramTable
+        ): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(
+            oldItem: ProgramTable,
+            newItem: ProgramTable
+        ): Boolean {
+            return oldItem == newItem
+        }
 
     }
 }
