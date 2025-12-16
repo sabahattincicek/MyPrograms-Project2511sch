7adc9d45ebc373390254156e374f4ba6a4e6037e
zabo0 <scicek12345@gmail.com>
2025-12-16 21:19:21 +0300

    refactor: add file repository provider in RepositoryModule.kt

--- app/src/main/java/com/saboon/project_2511sch/di/RepositoryModule.kt
+++ app/src/main/java/com/saboon/project_2511sch/di/RepositoryModule.kt
@@ -3,17 +3,20 @@ package com.saboon.project_2511sch.di
 import android.content.Context
 import com.saboon.project_2511sch.data.alarm.AlarmSchedulerImp
 import com.saboon.project_2511sch.data.local.dao.CourseDao
+import com.saboon.project_2511sch.data.local.dao.FileDao
 import com.saboon.project_2511sch.data.local.dao.ProgramTableDao
 import com.saboon.project_2511sch.data.local.dao.ScheduleDao
 import com.saboon.project_2511sch.data.local.dao.UserDao
 import com.saboon.project_2511sch.data.repository.AuthRepositoryImp
 import com.saboon.project_2511sch.data.repository.CourseRepositoryImp
+import com.saboon.project_2511sch.data.repository.FileRepositoryImp
 import com.saboon.project_2511sch.data.repository.ProgramTableRepositoryImp
 import com.saboon.project_2511sch.data.repository.ScheduleRepositoryImp
 import com.saboon.project_2511sch.data.repository.UserRepositoryImp
 import com.saboon.project_2511sch.domain.alarm.IAlarmScheduler
 import com.saboon.project_2511sch.domain.repository.IAuthRepository
 import com.saboon.project_2511sch.domain.repository.ICourseRepository
+import com.saboon.project_2511sch.domain.repository.IFileRepository
 import com.saboon.project_2511sch.domain.repository.IProgramTableRepository
 import com.saboon.project_2511sch.domain.repository.IScheduleRepository
 import com.saboon.project_2511sch.domain.repository.IUserRepository
@@ -64,6 +67,11 @@ object RepositoryModule {
     fun provideAlarmScheduler(@ApplicationContext context: Context): IAlarmScheduler{
         return AlarmSchedulerImp(context)
     }
-    
+
+    @Singleton
+    @Provides
+    fun provideFileRepositoryImp(fileDao: FileDao): IFileRepository {
+        return FileRepositoryImp(fileDao)
+    }
 
 }
