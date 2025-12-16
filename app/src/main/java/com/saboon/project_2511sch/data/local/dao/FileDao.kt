91aea9e524475aee93da996f06e74450a544aea3
zabo0 <scicek12345@gmail.com>
2025-12-16 20:36:41 +0300

    feat: create FileDao.kt

---
+++ app/src/main/java/com/saboon/project_2511sch/data/local/dao/FileDao.kt
@@ -0,0 +1,12 @@
+package com.saboon.project_2511sch.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import com.saboon.project_2511sch.domain.model.File
+
+@Dao
+interface FileDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(file: File)
+}
