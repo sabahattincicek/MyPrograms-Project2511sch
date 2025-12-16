42fdca323e26a7c3d745673e56e884bff55cbe68
zabo0 <scicek12345@gmail.com>
2025-12-16 20:34:18 +0300

    refactor: add FileEntity.kt in Database.kt

--- app/src/main/java/com/saboon/project_2511sch/data/local/database/Database.kt
+++ app/src/main/java/com/saboon/project_2511sch/data/local/database/Database.kt
@@ -7,13 +7,20 @@ import com.saboon.project_2511sch.data.local.dao.ProgramTableDao
 import com.saboon.project_2511sch.data.local.dao.ScheduleDao
 import com.saboon.project_2511sch.data.local.dao.UserDao
 import com.saboon.project_2511sch.data.local.entity.CourseEntity
+import com.saboon.project_2511sch.data.local.entity.FileEntity
 import com.saboon.project_2511sch.data.local.entity.ProgramTableEntity
 import com.saboon.project_2511sch.data.local.entity.ScheduleEntity
 import com.saboon.project_2511sch.data.local.entity.UserEntity
 
 @Database(
-    entities = [UserEntity::class, ProgramTableEntity::class, CourseEntity::class, ScheduleEntity::class],
-    version = 1,
+    entities = [
+        UserEntity::class,
+        ProgramTableEntity::class,
+        CourseEntity::class,
+        ScheduleEntity::class,
+        FileEntity::class
+    ],
+    version = 2,
     exportSchema = false)
 abstract class Database(): RoomDatabase() {
     abstract fun userDao(): UserDao
