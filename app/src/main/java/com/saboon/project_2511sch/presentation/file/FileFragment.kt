15ab9827fba552691cf24e709413a61271717c8f
zabo0 <scicek12345@gmail.com>
2025-12-16 21:56:20 +0300

    feat: create views for file fragment

---
+++ app/src/main/java/com/saboon/project_2511sch/presentation/file/FileFragment.kt
@@ -0,0 +1,39 @@
+package com.saboon.project_2511sch.presentation.file
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.saboon.project_2511sch.R
+import com.saboon.project_2511sch.databinding.FragmentFileBinding
+
+class FileFragment : Fragment() {
+
+    private var _binding: FragmentFileBinding?=null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentFileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
