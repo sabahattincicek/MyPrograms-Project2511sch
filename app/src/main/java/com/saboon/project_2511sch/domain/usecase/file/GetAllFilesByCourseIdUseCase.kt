f0ae4adc74b1ed94fd00d82b69cff309f8e0baf2
zabo0 <scicek12345@gmail.com>
2025-12-17 22:43:53 +0300

    feat: create get files function in viewModel

--- app/src/main/java/com/saboon/project_2511sch/domain/usecase/file/GetAllFilesByCourseIdUseCase.kt
+++ app/src/main/java/com/saboon/project_2511sch/domain/usecase/file/GetAllFilesByCourseIdUseCase.kt
@@ -1,13 +1,12 @@
 package com.saboon.project_2511sch.domain.usecase.file
 
-import android.R
 import com.saboon.project_2511sch.domain.model.File
 import com.saboon.project_2511sch.domain.repository.IFileRepository
 import com.saboon.project_2511sch.util.Resource
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class GetAllFilesByCourseId @Inject constructor(
+class GetAllFilesByCourseIdUseCase @Inject constructor(
     private val fileRepository: IFileRepository
 ) {
     operator fun invoke(id: String): Flow<Resource<List<File>>>{
--- app/src/main/java/com/saboon/project_2511sch/presentation/file/ViewModelFile.kt
+++ app/src/main/java/com/saboon/project_2511sch/presentation/file/ViewModelFile.kt
@@ -3,20 +3,27 @@ package com.saboon.project_2511sch.presentation.file
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.saboon.project_2511sch.domain.model.File
+import com.saboon.project_2511sch.domain.usecase.file.GetAllFilesByCourseIdUseCase
 import com.saboon.project_2511sch.domain.usecase.file.InsertNewFileUseCase
 import com.saboon.project_2511sch.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ViewModelFile @Inject constructor(
-    private val insertNewFileUseCase: InsertNewFileUseCase
+    private val insertNewFileUseCase: InsertNewFileUseCase,
+    private val getAllFilesByCourseIdUseCase: GetAllFilesByCourseIdUseCase
 ) : ViewModel() {
     private val _insertNewFileEvent = Channel<Resource<File>>()
     val insertNewFileEvent = _insertNewFileEvent.receiveAsFlow()
+
+    private val _filesState = MutableStateFlow<Resource<List<File>>>(Resource.Idle())
+    val filesState = _filesState.asStateFlow()
 
     fun insertNewFile(file: File){
         viewModelScope.launch {
@@ -26,6 +33,19 @@ class ViewModelFile @Inject constructor(
                 _insertNewFileEvent.send(result)
             }catch (e: Exception){
                 _insertNewFileEvent.send(Resource.Error(e.localizedMessage?:"An unexpected error occurred in ViewModel."))
+            }
+        }
+    }
+
+    fun getAllFilesByCourseId(id: String){
+        viewModelScope.launch {
+            try {
+                _filesState.value = Resource.Loading()
+                getAllFilesByCourseIdUseCase.invoke(id).collect { resource ->
+                    _filesState.value = resource
+                }
+            }catch (e: Exception){
+                _filesState.value = Resource.Error(e.localizedMessage?:"An unexpected error occurred in ViewModel.")
             }
         }
     }
