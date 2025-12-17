22bce35bf3caf3df79336c5379926c8cac2e437a
zabo0 <scicek12345@gmail.com>
2025-12-17 20:17:55 +0300

    feat: get args from navargs

--- app/src/main/java/com/saboon/project_2511sch/presentation/file/FileFragment.kt
+++ app/src/main/java/com/saboon/project_2511sch/presentation/file/FileFragment.kt
@@ -5,13 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.saboon.project_2511sch.R
 import com.saboon.project_2511sch.databinding.FragmentFileBinding
+import com.saboon.project_2511sch.domain.model.Course
 
 class FileFragment : Fragment() {
 
     private var _binding: FragmentFileBinding?=null
     private val binding get() = _binding!!
+
+    private val args : FileFragmentArgs by navArgs()
+
+    private lateinit var course: Course
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +36,7 @@ class FileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        course = args.course
     }
 
     override fun onDestroyView() {
