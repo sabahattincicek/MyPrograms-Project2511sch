3e31fff1a84c42ae8ae1eab8a64bd4e068e9b3a2
zabo0 <scicek12345@gmail.com>
2025-12-17 22:33:35 +0300

    feat: implement function in FileRepository

--- app/src/main/java/com/saboon/project_2511sch/data/repository/FileRepositoryImp.kt
+++ app/src/main/java/com/saboon/project_2511sch/data/repository/FileRepositoryImp.kt
@@ -1,10 +1,15 @@
 package com.saboon.project_2511sch.data.repository
 
 import com.saboon.project_2511sch.data.local.dao.FileDao
+import com.saboon.project_2511sch.data.local.entity.FileEntity
+import com.saboon.project_2511sch.data.local.mapper.toDomain
 import com.saboon.project_2511sch.data.local.mapper.toEntity
 import com.saboon.project_2511sch.domain.model.File
 import com.saboon.project_2511sch.domain.repository.IFileRepository
 import com.saboon.project_2511sch.util.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class FileRepositoryImp @Inject constructor(
@@ -17,5 +22,15 @@ class FileRepositoryImp @Inject constructor(
         }catch (e: Exception){
            return Resource.Error(e.localizedMessage?:"An unexpected error occurred")
         }
+    }
+
+    override fun getFilesByCourseId(id: String): Flow<Resource<List<File>>> {
+        return fileDao.getFilesByCourseId(id)
+            .map<List<FileEntity>, Resource<List<File>>> { entities ->
+                Resource.Success( entities.map { it.toDomain() } )
+            }
+            .catch { e ->
+                emit(Resource.Error(e.localizedMessage?:"An unexpected error occurred"))
+            }
     }
 }
