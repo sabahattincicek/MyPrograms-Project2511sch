0a3ab48db5ddf0632f24c5456042d78b5968ca20
zabo0 <scicek12345@gmail.com>
2025-12-18 21:33:18 +0300

    feat: added LOGs in ViewModelFile.kt

--- app/src/main/java/com/saboon/project_2511sch/presentation/file/ViewModelFile.kt
+++ app/src/main/java/com/saboon/project_2511sch/presentation/file/ViewModelFile.kt
@@ -1,5 +1,6 @@
 package com.saboon.project_2511sch.presentation.file
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.saboon.project_2511sch.domain.model.File
@@ -19,33 +20,44 @@ class ViewModelFile @Inject constructor(
     private val insertNewFileUseCase: InsertNewFileUseCase,
     private val getAllFilesByCourseIdUseCase: GetAllFilesByCourseIdUseCase
 ) : ViewModel() {
+
+    private val TAG = "ViewModelFile"
+
     private val _insertNewFileEvent = Channel<Resource<File>>()
     val insertNewFileEvent = _insertNewFileEvent.receiveAsFlow()
 
     private val _filesState = MutableStateFlow<Resource<List<File>>>(Resource.Idle())
     val filesState = _filesState.asStateFlow()
 
-    fun insertNewFile(file: File){
+    fun insertNewFile(file: File) {
+        Log.d(TAG, "insertNewFile: called with file title: ${file.title}")
         viewModelScope.launch {
             try {
+                Log.d(TAG, "insertNewFile: Sending Loading state.")
                 _insertNewFileEvent.send(Resource.Loading())
                 val result = insertNewFileUseCase.invoke(file)
+                Log.i(TAG, "insertNewFile: Received result from UseCase: $result")
                 _insertNewFileEvent.send(result)
-            }catch (e: Exception){
-                _insertNewFileEvent.send(Resource.Error(e.localizedMessage?:"An unexpected error occurred in ViewModel."))
+            } catch (e: Exception) {
+                Log.e(TAG, "insertNewFile: Exception caught while inserting file.", e)
+                _insertNewFileEvent.send(Resource.Error(e.localizedMessage ?: "An unexpected error occurred in ViewModel."))
             }
         }
     }
 
-    fun getAllFilesByCourseId(id: String){
+    fun getAllFilesByCourseId(id: String) {
+        Log.d(TAG, "getAllFilesByCourseId: called with course ID: $id")
         viewModelScope.launch {
             try {
+                Log.d(TAG, "getAllFilesByCourseId: Setting Loading state.")
                 _filesState.value = Resource.Loading()
                 getAllFilesByCourseIdUseCase.invoke(id).collect { resource ->
+                    Log.d(TAG, "getAllFilesByCourseId: Collected new resource for filesState: $resource")
                     _filesState.value = resource
                 }
-            }catch (e: Exception){
-                _filesState.value = Resource.Error(e.localizedMessage?:"An unexpected error occurred in ViewModel.")
+            } catch (e: Exception) {
+                Log.e(TAG, "getAllFilesByCourseId: Exception caught while getting files.", e)
+                _filesState.value = Resource.Error(e.localizedMessage ?: "An unexpected error occurred in ViewModel.")
             }
         }
     }
