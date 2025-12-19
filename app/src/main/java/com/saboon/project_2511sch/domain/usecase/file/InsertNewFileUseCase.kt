4ffeab210fa2c888dcf74055049015ebbea4aa06
zabo0 <scicek12345@gmail.com>
2025-12-19 21:55:34 +0300

    refactor: change insertFileUseCase parameter

--- app/src/main/java/com/saboon/project_2511sch/domain/usecase/file/InsertNewFileUseCase.kt
+++ app/src/main/java/com/saboon/project_2511sch/domain/usecase/file/InsertNewFileUseCase.kt
@@ -1,5 +1,6 @@
 package com.saboon.project_2511sch.domain.usecase.file
 
+import android.net.Uri
 import com.saboon.project_2511sch.domain.model.File
 import com.saboon.project_2511sch.domain.repository.IFileRepository
 import com.saboon.project_2511sch.util.Resource
@@ -8,7 +9,7 @@ import javax.inject.Inject
 class InsertNewFileUseCase @Inject constructor(
     private val fileRepository: IFileRepository
 ) {
-    suspend operator fun invoke(file: File): Resource<File>{
-        return fileRepository.insertFile(file)
+    suspend operator fun invoke(file: File, uri: Uri): Resource<File>{
+        return fileRepository.insertFile(file, uri)
     }
 }
