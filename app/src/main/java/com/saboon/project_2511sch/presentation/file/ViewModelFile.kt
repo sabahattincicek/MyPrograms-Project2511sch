2a8b1577e737dc15c87c20b7069dd830648aa1b4
zabo0 <scicek12345@gmail.com>
2025-12-19 20:19:35 +0300

    feat: create updateFile function in viewmodel

--- app/src/main/java/com/saboon/project_2511sch/presentation/file/ViewModelFile.kt
+++ app/src/main/java/com/saboon/project_2511sch/presentation/file/ViewModelFile.kt
@@ -7,6 +7,7 @@ import com.saboon.project_2511sch.domain.model.File
 import com.saboon.project_2511sch.domain.usecase.file.DeleteFileUseCase
 import com.saboon.project_2511sch.domain.usecase.file.GetAllFilesByCourseIdUseCase
 import com.saboon.project_2511sch.domain.usecase.file.InsertNewFileUseCase
+import com.saboon.project_2511sch.domain.usecase.file.UpdateFileUseCase
 import com.saboon.project_2511sch.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
@@ -20,7 +21,8 @@ import javax.inject.Inject
 class ViewModelFile @Inject constructor(
     private val insertNewFileUseCase: InsertNewFileUseCase,
     private val getAllFilesByCourseIdUseCase: GetAllFilesByCourseIdUseCase,
-    private val deleteFileUseCase: DeleteFileUseCase
+    private val deleteFileUseCase: DeleteFileUseCase,
+    private val updateFileUseCase: UpdateFileUseCase,
 ) : ViewModel() {
 
     private val TAG = "ViewModelFile"
@@ -30,6 +32,9 @@ class ViewModelFile @Inject constructor(
 
     private val _deleteFileEvent = Channel<Resource<File>>()
     val deleteFileEvent = _deleteFileEvent.receiveAsFlow()
+
+    private val _updateFileEvent = Channel<Resource<File>>()
+    val updateFileEvent = _updateFileEvent.receiveAsFlow()
 
     private val _filesState = MutableStateFlow<Resource<List<File>>>(Resource.Idle())
     val filesState = _filesState.asStateFlow()
@@ -58,6 +63,18 @@ class ViewModelFile @Inject constructor(
                 _deleteFileEvent.send(result)
             }catch (e: Exception){
                 _deleteFileEvent.send(Resource.Error(e.localizedMessage ?: "An unexpected error occurred in ViewModel."))
+            }
+        }
+    }
+
+    fun updateFile(file: File){
+        viewModelScope.launch {
+            try {
+                _updateFileEvent.send(Resource.Loading())
+                val result = updateFileUseCase.invoke(file)
+                _updateFileEvent.send(result)
+            }catch (e: Exception){
+                _updateFileEvent.send(Resource.Error(e.localizedMessage ?: "An unexpected error occurred in ViewModel."))
             }
         }
     }
