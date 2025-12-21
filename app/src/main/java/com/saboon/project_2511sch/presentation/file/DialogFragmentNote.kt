a2aea7a027ef24ccef77b4bd177af342078a1234
zabo0 <scicek12345@gmail.com>
2025-12-21 22:27:12 +0300

    refactor: change parameters of newInstance function

--- app/src/main/java/com/saboon/project_2511sch/presentation/file/DialogFragmentNote.kt
+++ app/src/main/java/com/saboon/project_2511sch/presentation/file/DialogFragmentNote.kt
@@ -25,7 +25,6 @@ class DialogFragmentNote: DialogFragment() {
     private val binding get() = _binding!!
 
     private lateinit var course: Course
-    private var uri: Uri? = null
     private var file: File? = null
 
     override fun onCreateView(
@@ -47,7 +46,6 @@ class DialogFragmentNote: DialogFragment() {
 
         arguments?.let {
             course = BundleCompat.getParcelable(it, ARG_COURSE, Course::class.java)!!
-            uri = BundleCompat.getParcelable(it, ARG_URI, Uri::class.java)
             file = BundleCompat.getParcelable(it, ARG_FILE, File::class.java)
         }
 
@@ -117,13 +115,11 @@ class DialogFragmentNote: DialogFragment() {
         const val REQUEST_KEY_CREATE = "note_dialog_fragment_request_key_create"
         const val REQUEST_KEY_UPDATE = "note_dialog_fragment_request_key_update"
         const val RESULT_KEY_FILE = "note_dialog_fragment_result_key_file"
-        const val RESULT_KEY_URI = "note_dialog_fragment_result_key_uri"
 
-        fun newInstance(course: Course, uri: Uri?, file: File?): DialogFragmentNote{
+        fun newInstance(course: Course, file: File?): DialogFragmentNote{
             val fragment = DialogFragmentNote()
             fragment.arguments = bundleOf(
                 ARG_COURSE to course,
-                ARG_URI to uri,
                 ARG_FILE to file
             )
             return fragment
