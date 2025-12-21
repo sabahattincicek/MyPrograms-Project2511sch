dd0efadb43b2a7efb6def05c9ef6660af3051d42
zabo0 <scicek12345@gmail.com>
2025-12-21 20:32:30 +0300

    chore: added rich text editor dependecies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,4 +93,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:${lifecycle_version}")
     implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:${lifecycle_version}")
     implementation("androidx.fragment:fragment-ktx:1.8.9")
+
+    //rich text
+    implementation("jp.wasabeef:richeditor-android:2.0.0")
 }
