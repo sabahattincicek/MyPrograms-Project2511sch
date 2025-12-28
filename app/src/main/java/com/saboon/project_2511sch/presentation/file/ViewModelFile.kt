e8fb598e4bfc20cb0358194a731ae0c0d1360471
zabo0 <scicek12345@gmail.com>
2025-12-28 21:56:09 +0300

    create get all files function in view model

--- app/src/main/java/com/saboon/project_2511sch/presentation/file/ViewModelFile.kt
+++ app/src/main/java/com/saboon/project_2511sch/presentation/file/ViewModelFile.kt
@@ -1,11 +1,33 @@
 package com.saboon.project_2511sch.presentation.file
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.saboon.project_2511sch.domain.model.File
+import com.saboon.project_2511sch.domain.usecase.file.GetAllFilesUseCase
+import com.saboon.project_2511sch.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ViewModelFile @Inject constructor(
-
+    private val getAllFilesUseCase: GetAllFilesUseCase
 ): ViewModel() {
+    private var _filesState = MutableStateFlow<Resource<List<File>>>(Resource.Idle())
+    val filesState = _filesState.asStateFlow()
+
+    fun getAllFiles(){
+        viewModelScope.launch {
+            try {
+                _filesState.value = Resource.Loading()
+                val result = getAllFilesUseCase.invoke().collect { resource ->
+                    _filesState.value = resource
+                }
+            }catch (e: Exception){
+                _filesState.value = Resource.Error(e.localizedMessage ?: "An unexpected error occurred in ViewModel.")
+            }
+        }
+    }
 }
