97e975ea6e1c461fe066901b4bc0adc880e58ee2
zabo0 <scicek12345@gmail.com>
2025-12-28 22:20:00 +0300

    create observer function for get all files in fragment

--- app/src/main/java/com/saboon/project_2511sch/presentation/file/FileFragment.kt
+++ app/src/main/java/com/saboon/project_2511sch/presentation/file/FileFragment.kt
@@ -5,14 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.saboon.project_2511sch.R
 import com.saboon.project_2511sch.databinding.FragmentFileBinding
+import com.saboon.project_2511sch.util.Resource
+import kotlinx.coroutines.launch
 
 
 class FileFragment : Fragment() {
 
     private var _binding : FragmentFileBinding?=null
     private val binding get() = _binding!!
+
+    private val viewModelFile: ViewModelFile by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,5 +45,22 @@ class FileFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun observeFilesState(){
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED){
+                viewModelFile.filesState.collect { resource ->
+                    when(resource) {
+                        is Resource.Error<*> -> {}
+                        is Resource.Idle<*> -> {}
+                        is Resource.Loading<*> -> {}
+                        is Resource.Success<*> -> {
+                            // TODO: set file list to recycler adapter
+                        }
+                    }
+                }
+            }
+        }
     }
 }
