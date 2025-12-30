c97fe7ac1ae77c8a27c9ca7d895d1124c893fa94
zabo0 <scicek12345@gmail.com>
2025-12-30 23:23:59 +0300

    feat: create profile layout

---
+++ app/src/main/java/com/saboon/project_2511sch/presentation/profile/ProfileFragment.kt
@@ -0,0 +1,29 @@
+package com.saboon.project_2511sch.presentation.profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.saboon.project_2511sch.R
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class ProfileFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+
+}
