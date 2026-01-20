e5189a721bf3b6e1563353de854867d2d42d694b
sabahattincicek <scicek12345@gmail.com>
2026-01-20 22:23:04 +0300

    feat: add item checked listener in RecyclerAdapterDialogFragmentProgramTableSelector.kt

--- app/src/main/java/com/saboon/project_2511sch/presentation/home/RecyclerAdapterDialogFragmentProgramTableSelector.kt
+++ app/src/main/java/com/saboon/project_2511sch/presentation/home/RecyclerAdapterDialogFragmentProgramTableSelector.kt
@@ -5,7 +5,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.saboon.project_2511sch.databinding.RecyclerListRowDialogFragmentHomeBinding
+import com.saboon.project_2511sch.databinding.RecyclerListRowProgramTableSelectorBinding
 import com.saboon.project_2511sch.domain.model.ProgramTable
 
 class RecyclerAdapterDialogFragmentProgramTableSelector : ListAdapter<ProgramTable, RecyclerAdapterDialogFragmentProgramTableSelector.DialogFragmentHomeViewHolder>(
@@ -13,11 +13,17 @@ class RecyclerAdapterDialogFragmentProgramTableSelector : ListAdapter<ProgramTab
 ) {
 
     var onItemClickListener: ((ProgramTable) -> Unit)? = null
+    var onItemCheckedChangeListener: ((ProgramTable) -> Unit)? = null
 
-    class DialogFragmentHomeViewHolder(private val binding: RecyclerListRowDialogFragmentHomeBinding): RecyclerView.ViewHolder(binding.root) {
+    inner class DialogFragmentHomeViewHolder(private val binding: RecyclerListRowProgramTableSelectorBinding): RecyclerView.ViewHolder(binding.root) {
         fun bind(item: ProgramTable){
             binding.tvProgramTableTitle.text = item.title
             binding.tvProgramTableDescription.text = item.description
+            binding.ckbProgramTable.setOnCheckedChangeListener { buttonView, isChecked ->
+                if (isChecked){
+                    onItemCheckedChangeListener?.invoke(item)
+                }
+            }
         }
     }
 
@@ -25,16 +31,13 @@ class RecyclerAdapterDialogFragmentProgramTableSelector : ListAdapter<ProgramTab
         parent: ViewGroup,
         viewType: Int
     ): DialogFragmentHomeViewHolder {
-        val binding = RecyclerListRowDialogFragmentHomeBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding = RecyclerListRowProgramTableSelectorBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return DialogFragmentHomeViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: DialogFragmentHomeViewHolder, position: Int) {
         val item = getItem(position)
         holder.bind(item)
-        holder.itemView.setOnClickListener {
-            onItemClickListener?.invoke(item)
-        }
     }
 
     open class DialogFragmentHomeDiffCallback: DiffUtil.ItemCallback<ProgramTable>() {
